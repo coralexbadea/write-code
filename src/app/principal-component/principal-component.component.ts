6e5ca26770330b76445e73bef25ab183aa263be3
coralexbadea <coralexbadea99@gmail.com>
2023-04-16 18:25:00 +0300

    hope

--- src/app/principal-component/principal-component.component.ts
+++ src/app/principal-component/principal-component.component.ts
@@ -11,7 +11,7 @@ export class PrincipalComponentComponent implements OnInit {
 
   @ViewChildren('inputTag') inputElem: ElementRef | undefined;
 
-  firstFC = new FormControl('');
+  firstFC = new FormControl('This is demo text for you to write');
   secondFC = new FormControl('');
   inputtingText = false;
   redText = "";
@@ -30,8 +30,8 @@ export class PrincipalComponentComponent implements OnInit {
 
 
   onTextChange() {
-    var len_second_string = this.secondFC.value.length
-    var len_first_string = this.firstFC.value.length
+    let len_second_string = this.secondFC.value.length
+    let len_first_string = this.firstFC.value.length
     let i;
     this.greenText = "";
     this.redText = "";
@@ -44,15 +44,14 @@ export class PrincipalComponentComponent implements OnInit {
       }
     }
     this.redText += this.secondFC.value.slice(i);
-    if(this.redText.length == 0) {
+    if(this.redText.length == 0 && this.greenText.length != 0) {
       this.finished = true;
     }
 
   }
 
   myTabFunction(event:any){
-    console.log("here")
-    if(event.keyCode===9){
+    if(event.keyCode==9){
       this.firstFC.setValue(this.firstFC.value + "    ");
       return false;
     }
